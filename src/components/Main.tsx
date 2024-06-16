351c305f6feaf966e84e035109f6c4293495574a
Wenjian(Michael) Shi <61084115+Micsushi@users.noreply.github.com>
2024-06-16 00:26:52 -0600

    Initial commit

---
+++ src/components/Main.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import '../assets/styles/Main.scss';
+
+function Main() {
+  const image = process.env.PUBLIC_URL + '/selfie.jpg';
+  return (
+    <div className="container">
+      <div className="about-section">
+        <div className="image-wrapper">
+          <img src={image} alt="Avatar" />
+        </div>
+        <div className="content">
+          <div className="social_icons">
+            <a href="https://github.com/micsushi" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/wjshi/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+          </div>
+          <h1>Michael Shi</h1>
+          <p className="title">Full Stack Developer</p>
+
+          <div className="mobile_social_icons">
+            <a href="https://github.com/micsushi" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/wjshi/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
